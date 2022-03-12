f46159512bc98685834d995e30fc1618fb1fa362
seven-mile <i@7li.moe>
2022-03-12 13:59:30 +0800

    fix(refactor): indent size = 2

--- UFCase/CbsProviderManager.h
+++ UFCase/CbsProviderManager.h
@@ -9,32 +9,32 @@
 
 namespace winrt::UFCase {
 
-	enum class CbsProvidePolicy {
-		Singleton,
-		Multiton
-	};
+    enum class CbsProvidePolicy {
+        Singleton,
+        Multiton
+    };
 
-	class ICbsProvider {
-	public:
-		virtual CbsProvidePolicy GetProvidePolicy() const = 0;
-		virtual std::filesystem::path GetBootdrive() const = 0;
-		virtual winrt::com_ptr<ICbsSession> ApplySession() = 0;
-		virtual void Dispose() = 0;
-	};
+    class ICbsProvider {
+    public:
+        virtual CbsProvidePolicy GetProvidePolicy() const = 0;
+        virtual std::filesystem::path GetBootdrive() const = 0;
+        virtual winrt::com_ptr<ICbsSession> ApplySession() = 0;
+        virtual void Dispose() = 0;
+    };
 
-	class CbsProviderManager
-	{
-		std::map<std::filesystem::path, ICbsProvider*> m_provStore;
-		std::map<std::filesystem::path, std::unordered_set<winrt::hstring>> m_mapUsers;
+    class CbsProviderManager
+    {
+        std::map<std::filesystem::path, ICbsProvider*> m_provStore;
+        std::map<std::filesystem::path, std::unordered_set<winrt::hstring>> m_mapUsers;
 
-		CbsProviderManager();
-	public:
+        CbsProviderManager();
+    public:
 
-		static CbsProviderManager& Current();
-		static std::filesystem::path GetOnlineBootdrive();
+        static CbsProviderManager& Current();
+        static std::filesystem::path GetOnlineBootdrive();
 
-		ICbsProvider* ApplyFromBootdrive(winrt::hstring const& client, std::filesystem::path const& bootdrive, CbsProvidePolicy policy = CbsProvidePolicy::Singleton);
-		const void Return(winrt::hstring const& client, std::filesystem::path const& bootdrive);
-	};
+        ICbsProvider* ApplyFromBootdrive(winrt::hstring const& client, std::filesystem::path const& bootdrive, CbsProvidePolicy policy = CbsProvidePolicy::Singleton);
+        const void Return(winrt::hstring const& client, std::filesystem::path const& bootdrive);
+    };
 
 } // namespace winrt::UFCase
